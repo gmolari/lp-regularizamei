826a80bd6df0bb05b8dc171258bb140fa96550da
= <=>
2024-06-20 13:21:24 -0300

    hamburger

--- script.js
+++ script.js
@@ -9,8 +9,35 @@ document.addEventListener("DOMContentLoaded", function () {
     breakpoints: {
       640: {
         perView: 1,
-        gap: 0
+        gap: 0,
       },
     },
   }).mount();
+
+  const icon = document.querySelector(".menu-icon");
+
+  let isOpen = false;
+  let timeout;
+
+  icon.addEventListener("click", () => {
+    const menu = document.getElementById("header-list");
+    if (!isOpen) {
+      menu.classList.add("ascending");
+      menu.classList.remove("descending");
+      menu.style.display = "flex";
+      isOpen = true;
+    } else {
+      menu.classList.add("descending");
+      menu.classList.remove("ascending");
+      
+      if (!timeout) {
+        timeout = setTimeout(() => {
+          menu.style.display = "none";
+          timeout = undefined;
+        }, 180);
+      }
+
+      isOpen = false;
+    }
+  });
 });
